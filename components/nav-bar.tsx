0197e131e4fdabd5ab70f39f9bc65bd47ba02249
Antonio Perdomo <110626235+AntonioMPSS@users.noreply.github.com>
2024-11-14 23:10:09 -0600

    feat: update font and add mobile styles

--- components/nav-bar.tsx
+++ components/nav-bar.tsx
@@ -1,83 +1,90 @@
+'use client';
+
 import Image from "next/image";
 import Link from "next/link";
-import { FaChevronDown } from "react-icons/fa";
+import { useState } from "react";
+import { FaBars, FaChevronDown, FaPlus } from "react-icons/fa";
 
 export default function NavBar() {
   const linkStyle =
     "text-[16px] text-dimgray py-[18px] px-[16px] mx-1 font-semibold uppercase rounded transition-all ease-in delay-100 hover:text-white hover:bg-midnightblue";
+  const mobileLinkStyle = "";
+  const [openMenu, setOpenMenu] = useState(false);
 
   return (
-    <nav className="bg-white flex justify-between items-center py-5 px-14 w-full flex-wrap">
-      <Link href={"/"}>
-        <Image
-          src="/icon/fixup.svg"
-          height={59}
-          width={139}
-          alt={"Fixup Roofing & Construction Logo"}
-        />
-      </Link>
-      <div className="px-8 flex flex-wrap">
-        <Link href={"/about"} className={linkStyle}>
-          About Us
+    <nav className="bg-white flex items-center w-full flex-wrap">
+      <div className="flex flex-wrap justify-between items-center w-full py-5 px-14 relative">
+        <Link href={"/"}>
+          <Image
+            src="/icon/fixup.svg"
+            height={59}
+            width={139}
+            alt={"Fixup Roofing & Construction Logo"}
+          />
         </Link>
-        <div className={`services-section relative ${linkStyle}`}>
-          <Link href={"/services"} className="flex items-center">
-            Services <FaChevronDown className="text-xs ml-2" />
+        <div className="px-8 hidden xl:flex flex-wrap">
+          <Link href={"/about"} className={linkStyle}>
+            About Us
           </Link>
-          {/* <div className="services absolute bg-midnightblue text-white rounded uppercase px-6 hover:flex flex-col w-[240px] top-0 left-0 hidden">
-            <Link href={"/services/air-conditioning"} className="x">
-              Air Conditioning
+          <div className={`services-section relative ${linkStyle}`}>
+            <Link href={"/services"} className="flex items-center">
+              Services <FaChevronDown className="text-xs ml-2" />
             </Link>
-            <Link href={"/services/commercial-roofing"} className="x">
-              Commercial Roofing 
-            </Link>
-            <Link href={"/services/residential-roofing"} className="x">
-              Residential Roofing
-            </Link>
-            <Link href={"/services/solar-panels"} className="x">
-              Solar Panels 
-            </Link>
-            <Link href={"/services/windows-and-doors"} className="x">
-              Windows and Doors 
-            </Link>
-          </div> */}
+          </div>
+          <Link
+            href={"/locations"}
+            className={`${linkStyle}  flex items-center`}
+          >
+            Locations <FaChevronDown className="text-xs ml-2" />
+          </Link>
+          <Link href={"/financing"} className={linkStyle}>
+            Financing
+          </Link>
+          <Link href={"/projects"} className={linkStyle}>
+            Projects
+          </Link>
+          <Link href={"/blog"} className={linkStyle}>
+            Blog
+          </Link>
         </div>
-        <Link href={"/locations"} className={`${linkStyle}  flex items-center`}>
-          Locations <FaChevronDown className="text-xs ml-2" />
-        </Link>
-        {/* <div className="locations absolute bg-midnightblue text-white rounded uppercase px-6 flex flex-col w-[240px]">
-          <Link href={"/services/fort-lauderdale"} className="x">
-            Fort Lauderdale
+        <div className="free-estimate flex items-center">
+          <button className="bg-midnightblue block xl:hidden py-[7px] px-[5px] mr-6 rounded hover:bg-dimgray" onClick={() => setOpenMenu(!openMenu)}>
+            <FaBars className={`${openMenu ? 'hidden' : 'block'} w-6 h-5`} />
+            <FaPlus className={`${openMenu ? 'block' : 'hidden'} rotate-45 w-6 h-5`} />
+          </button>
+          <Link
+            href={"/estimates"}
+            className="text-[16px] p-[18px] text-white bg-forestgreen rounded uppercase font-semibold transition-all ease-in delay-100 hover:bg-midnightblue"
+          >
+            Free Estimates
           </Link>
-          <Link href={"/services/hollywood"} className="x">
-            Hollywood 
-          </Link>
-          <Link href={"/services/miramar"} className="x">
-            Miramar 
-          </Link>
-          <Link href={"/services/pembroke-pines"} className="x">
-            Pembroke Pines 
-          </Link>
-          <Link href={"/services/pompano-beach"} className="x">
-            Pompano Beach 
-          </Link>
-        </div> */}
-        <Link href={"/financing"} className={linkStyle}>
-          Financing
-        </Link>
-        <Link href={"/projects"} className={linkStyle}>
-          Projects
-        </Link>
-        <Link href={"/blog"} className={linkStyle}>
-          Blog
-        </Link>
+        </div>
       </div>
-      <Link
-        href={"/estimates"}
-        className="text-[16px] p-[18px] text-white bg-forestgreen rounded uppercase font-semibold transition-all ease-in delay-100 hover:bg-midnightblue"
-      >
-        Free Estimates
-      </Link>
+      <div className="mobile-menu w-full flex xl:hidden bg-midnightblue flex-col ">
+          <Link href={"/about"} className={mobileLinkStyle}>
+            About Us
+          </Link>
+          <div className={`services-section relative ${mobileLinkStyle}`}>
+            <Link href={"/services"} className="flex items-center">
+              Services <FaChevronDown className="text-xs ml-2" />
+            </Link>
+          </div>
+          <Link
+            href={"/locations"}
+            className={`${mobileLinkStyle}  flex items-center`}
+          >
+            Locations <FaChevronDown className="text-xs ml-2" />
+          </Link>
+          <Link href={"/financing"} className={mobileLinkStyle}>
+            Financing
+          </Link>
+          <Link href={"/projects"} className={mobileLinkStyle}>
+            Projects
+          </Link>
+          <Link href={"/blog"} className={mobileLinkStyle}>
+            Blog
+          </Link>
+        </div>
     </nav>
   );
 }
