5c2f36f51b808012e39eafa8559cf3ba1539ccf2
AntonioMPSS <110626235+AntonioMPSS@users.noreply.github.com>
2024-11-22 00:11:57 -0600

    feat: option section element completed

--- app/page.tsx
+++ app/page.tsx
@@ -3,8 +3,9 @@ import { getPage } from "@/utils/api";
 // import DynamicModule from "@/components/dynamic-module";
 import TopBar from "@/components/top-bar";
 import { PageData } from "../components/types";
-import TopSection from "@/components/top-section";
+// import TopSection from "@/components/top-section";
 import OptionSection from "@/components/option-section";
+import { faqs } from "@/utils/mock-data";
 
 export const revalidate = 60;
 
@@ -19,56 +20,28 @@ export default async function HomePage() {
   //   return <div>Not Found</div>;
   // }
 
-  const benefits = [
-    "Get the Best Value for Your Money",
-    "Enjoy Quick, Efficient Service",
-    "Maintain Your Peace of Mind!",
-  ];
+  // const {title, secondTitle, description, benefits, buttonLabel} = topRoofing; //Uncomment this section to see the TopSection data in its element
+  const {title, secondTitle, topDescription, options, middleDescription, bottomDescription} = faqs;
 
-  const faqs = {
-    title: "Picture the Relief of a Hassle-Free",
-    secondTitle: "Roof Over Your Head",
-    topDescription:
-      " Think for a moment about what it would be like if you could:",
-    options: [
-      {
-        label:
-          " Secure high-quality roofing services at affordable rates – no hidden costs.",
-      },
-      {
-        label:
-          " Harness the power of skilled professionals to protect your home or business.",
-      },
-      {
-        label:
-          " Fix any roofing issue quickly – no matter the season or the reason.",
-      },
-      {
-        label:
-          " Negotiate clear, straightforward terms that leave everyone satisfied.",
-      },
-    ],
-    middleDescription: "Doesn’t that sound uplifting?",
-    bottomDescription:
-      "Let us introduce you to our general roofing services which will empower you to enjoy all those advantages",
-  };
 
   return (
     <div>
-      <TopSection
-        title="Top Roofing Services in"
-        secondTitle="Miami for Reliable Repairs!"
-        description="Choose Fix Up Roofing and Construction for reliable roof services that fit your busy schedule and avoid repair headaches."
+      {/* 
+        // Uncomment this section to see the TopSection element 
+        <TopSection
+        title={title}
+        secondTitle={secondTitle}
+        description={description}
         benefits={benefits}
-        buttonLabel="Contact us today!"
-      />
+        buttonLabel={buttonLabel}
+      /> */}
       <OptionSection
-        title={faqs.title}
-        secondTitle={faqs.secondTitle}
-        topDescription={faqs.topDescription}
-        options={faqs.options}
-        middleDescription={faqs.middleDescription}
-        bottomDescription={faqs.bottomDescription}
+        title={title}
+        secondTitle={secondTitle}
+        topDescription={topDescription}
+        options={options}
+        middleDescription={middleDescription}
+        bottomDescription={bottomDescription}
       />
       {topBarData && (
         <TopBar
--- components/option-section.tsx
+++ components/option-section.tsx
@@ -22,11 +22,11 @@ export default function OptionSection({
 }: Props) {
   return (
     <section className="option-section bg-platinum w-full flex text-[14px] md:text-[15px] lg:text-[16px] font-normal text-black justify-center">
-      <div className="basis-10/12 w-10/12 py-[100] flex flex-col items-center">
-        <h1 className="text-[30px] md:text-[50px] font-bold uppercase leading-none text-center text-midnightblue">
+      <div className=" basis-11/12 w-11/12 lg:basis-10/12 lg:w-10/12 py-[100] flex flex-col items-center">
+        <h1 className="text-[30px] md:text-[50px] font-bold uppercase leading-none text-center text-midnightblue px-0 md:px-[50px] lg:px-0">
           {title} <span className="text-forestgreen">{secondTitle}</span>
         </h1>
-        {topDescription && <p>{topDescription}</p>}
+        {topDescription && <p className="text-center lg:text-left py-5 lg:p-5">{topDescription}</p>}
         <div className="flex flex-wrap">
           {options.map((option) => (
             <CheckWidget
@@ -36,8 +36,8 @@ export default function OptionSection({
             />
           ))}
         </div>
-        {middleDescription && <p>{middleDescription}</p>}
-        {bottomDescription && <p>{bottomDescription}</p>}
+        {middleDescription && <p className="pt-5 pb-[15px] text-center lg:text-left">{middleDescription}</p>}
+        {bottomDescription && <p className="text-center lg:text-left">{bottomDescription}</p>}
       </div>
     </section>
   );
