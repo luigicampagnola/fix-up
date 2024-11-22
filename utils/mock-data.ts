e088c3b72f7c73ab1f9891daa85e99e188ac4412
Antonio Perdomo <110626235+AntonioMPSS@users.noreply.github.com>
2024-11-22 07:49:11 -0600

    Feature: Option Section  (#3)
    * feat: added clsx and styles for the option section
    
    * feat: option section element completed

---
+++ utils/mock-data.ts
@@ -0,0 +1,41 @@
+/** *************** PROJECT MOCK DATA ***************** **/
+export const topRoofing = {
+    title:"Top Roofing Services in",
+    secondTitle:"Miami for Reliable Repairs!",
+    description:"Choose Fix Up Roofing and Construction for reliable roof services that fit your busy schedule and avoid repair headaches.",
+    benefits: [
+      "Get the Best Value for Your Money",
+      "Enjoy Quick, Efficient Service",
+      "Maintain Your Peace of Mind!",
+    ],
+    buttonLabel:"Contact us today!"
+};
+
+export const faqs = {
+    title: "Picture the Relief of a Hassle-Free",
+    secondTitle: "Roof Over Your Head",
+    topDescription:
+      " Think for a moment about what it would be like if you could:",
+    options: [
+      {
+        label:
+          " Secure high-quality roofing services at affordable rates – no hidden costs.",
+        description: " A roof typically lasts 25 to 50 years, depending on material quality and type, offering long-term protection for your home."
+      },
+      {
+        label:
+          " Harness the power of skilled professionals to protect your home or business.",
+      },
+      {
+        label:
+          " Fix any roofing issue quickly – no matter the season or the reason.",
+      },
+      {
+        label:
+          " Negotiate clear, straightforward terms that leave everyone satisfied.",
+      },
+    ],
+    middleDescription: "Doesn’t that sound uplifting?",
+    bottomDescription:
+      "Let us introduce you to our general roofing services which will empower you to enjoy all those advantages",
+  };
