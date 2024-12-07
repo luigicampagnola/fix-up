b08aa13d5e38eb23358c460dc2e26eb0d569a51c
Antonio Perdomo <110626235+AntonioMPSS@users.noreply.github.com>
2024-12-06 21:16:18 -0600

    refactor: removed extra pages and manage all pages in one site

--- utils/api.ts
+++ utils/api.ts
@@ -4,25 +4,26 @@ import configs from '../environment.configs';
 
 const { STRAPI_URL } = configs.development;
 
-export async function getPage(page: string) {
-  const res = await fetch(`${STRAPI_URL}${page}?populate[modules][populate]=*`);
+// export async function getPage(page: string) {
+//   const res = await fetch(`${STRAPI_URL}${page}?populate[modules][populate]=*`);
 
-  // const res = await fetch(`${STRAPI_API_URL}pages?filters[slug][$eq]=${slug}&populate[modules][populate]=*`);
+//   // const res = await fetch(`${STRAPI_API_URL}pages?filters[slug][$eq]=${slug}&populate[modules][populate]=*`);
+//   if (!res.ok) {
+//     throw new Error("Failed to fetch pages data from strapi");
+//   }
+//   const jsonData = await res.json();
+
+//   return jsonData[0];
+// }
+
+export async function getPage(slug: string) {
+  const res = await fetch(`${STRAPI_URL}pages?filters[slug][$eq]=${slug}&populate[modules][populate]=*`);
+
   if (!res.ok) {
     throw new Error("Failed to fetch pages data from strapi");
   }
+  
   const jsonData = await res.json();
 
   return jsonData[0];
-}
-
-export async function getSlug(page: string, slug: string) {
-  const res = await fetch(`${STRAPI_URL}${page}?filters[slug][$eq]=${slug}&populate[modules][populate]=*`);
-  if (!res.ok) {
-    throw new Error("Failed to fetch pages data from strapi");
-  }
-
-  const jsonData = await res.json();
-
-  return jsonData.data[0];
 }
