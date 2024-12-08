1ba3e98f8350256f6b94ae5e8ac9d23f05de41a7
AntonioMPSS <110626235+AntonioMPSS@users.noreply.github.com>
2024-12-08 13:56:18 -0600

    feat: google rate, information section component

---
+++ components/google-rate.tsx
@@ -0,0 +1,106 @@
+"use client";
+
+import Image from "next/image";
+import { Rates } from "./types";
+import { useState } from "react";
+import { FaAngleLeft, FaAngleRight, FaStar } from "react-icons/fa";
+
+type Props = {
+  rates: Rates;
+};
+
+export default function GoogleRate({ rates }: Props) {
+  const [slide, setSlide] = useState(0);
+  const [readMore, setReadMore] = useState(false);
+  const { comments, googleLogo, label, subLabel } = rates;
+  const readMoreLabel = readMore ? "Hide" : "Read more";
+
+  const nextSlide = () => {
+    if (rates && slide + 1 < rates.comments.length) {
+      setSlide(slide + 1);
+    }
+  };
+
+  const prevSlide = () => {
+    if (slide - 1 >= 0) {
+      setSlide(slide - 1);
+    }
+  };
+
+  return (
+    <div className="bg-white py-[15px] px-[20px] rounded-lg mx-[5px] mt-[-100px] relative z-10">
+      <p className="text-[14px] font-bold">{label}</p>
+      <p className="text-[14px]">{subLabel}</p>
+      <div className="flex justify-between pt-[10px] pb-[15px]">
+        <Image
+          className=""
+          src={googleLogo.url}
+          alt="google-logo"
+          width={90}
+          height={30}
+        />
+        {comments.map((comment, index) => (
+          <div
+            key={`star-${index}`}
+            className={`${index === slide ? "flex" : "hidden"}`}
+          >
+            {Array(comment.rate)
+              .fill("stars")
+              .map((star, index) => (
+                <FaStar
+                  key={`${star}-${index}`}
+                  className="text-yellow-400 text-[20px]"
+                />
+              ))}
+          </div>
+        ))}
+      </div>
+      <div className="rates">
+        {comments.map((comment, index) => {
+          const message = comment.comment;
+          const maxLength = 200;
+          const updatedText =
+            message.length > maxLength
+              ? message.substring(0, readMore ? message.length : maxLength)
+              : message;
+
+          return (
+            <div
+              key={`comment-${index}`}
+              className={`${
+                index === slide ? "flex" : "hidden"
+              } flex flex-col items-start`}
+            >
+              <p
+                className={`comment text-[15px] font-[500] border-t border-solid border-antiflaswhite pt-[10px] transition-all duration-500 ease-in-out overflow-hidden ${
+                  readMore ? "max-h-60" : "max-h-20"
+                }`}
+              >
+                {updatedText}
+                {readMore ? "" : "..."}
+              </p>
+              <button
+                className="text-[13.5px] text-chinesesilver"
+                onClick={() => setReadMore(!readMore)}
+              >
+                {readMoreLabel}
+              </button>
+            </div>
+          );
+        })}
+      </div>
+      <button
+        className="border-chinesesilver border border-solid rounded-full p-1 mr-1"
+        onClick={() => prevSlide()}
+      >
+        <FaAngleLeft className="text-chinesesilver" />
+      </button>
+      <button
+        className="border-chinesesilver border border-solid rounded-full p-1 ml-1"
+        onClick={() => nextSlide()}
+      >
+        <FaAngleRight className="text-chinesesilver" />
+      </button>
+    </div>
+  );
+}
--- utils/mock-data.ts
+++ utils/mock-data.ts
@@ -142,3 +142,33 @@ export const workingWithUsCards = {
     }
   ]
 };
+
+export const informationData = {
+  title: "Here's Our Cool",
+  subtitle: "Commitment to You...",
+  description: "<p><span style='font-weight:400;'>With </span><a href='https://fixuproofing.com/about-us/'><strong>over 14 years of experience</strong></a><span style='font-weight:400;'> in both the </span><a href='https://fixuproofing.com/services/commercial-roofing/'><strong>commercial</strong></a><span style='font-weight:400;'> and </span><a href='https://fixuproofing.com/services/residential-roofing/'><strong>residential</strong></a><span style='font-weight:400;'> sectors, we bring a storm of expertise to every project. We’ve navigated the challenges of Miami’s extreme weather, ensuring our clients enjoy </span><b>year-round comfort and efficiency.</b></p><p><a href='https://fixuproofing.com/about-us/'><strong>Our team</strong></a><span style='font-weight:400;'> has not only mastered the arts of installation and repair but also understands how to offer </span><b>the best in customer care</b><span style='font-weight:400;'>. We believe in building </span><b>lasting relationships</b><span style='font-weight:400;'> through </span><b>professional service, quality work</b><span style='font-weight:400;'>, and a genuine desire to make your life cooler and more comfortable.</span></p><p><span style='font-weight:400;'>Remember, whether you need a simple repair or a complete installation, Fix Up Roofing and Construction is here to ensure you can relax and stay cool. </span><b>Contact us today</b><span style='font-weight:400;'>—let’s put your AC troubles on ice!</span></p>",
+  image: { url: "https://cdn-ilbgjlh.nitrocdn.com/lIRNSdRIZIWyjDsnXOMEvdUxwLrZobol/assets/images/optimized/rev-7a46516/fixuproofing.com/wp-content/uploads/2024/06/2-air.jpg.webp" },
+  rates: {
+    label: 'EXCELLENT rating',
+    subLabel: 'Based on 133 reviews',
+    googleLogo: {url: 'https://cdn.trustindex.io/assets/platform/Google/logo.svg'},
+    comments: [
+      {
+        name: 'Luigi Campagnola',
+        date: '27 october 2024',
+        comment: 'I recently had the pleasure of working with Konstantinos Dimoudis at Fix Up roofing. and I couldn’t be happier with the results! From start to finish, their team was professional, courteous, and highly skilled. I was not able to be present during the project. But thanks to Konstantinos he was able to keep me informed all the way of what was happening I really appreciate his support and professionalism and for go above and beyond. He takes care everything for like it was his own house. And thank you to staff to that worked on the roof, what impressed me the most was their attention to detail and commitment to cleanliness. They made sure to protect my property and cleaned up thoroughly after the job was done. The new roof looks fantastic, and I’ve already received compliments from my neighbors. I  highly recommend this company but especially as for “Konstantinos” to anyone in need of roofing services. Their exceptional workmanship and customer service truly set them apart. Thank you for a job well done!',
+        rate: 4
+      },
+      {
+        name: 'Antonio Perdomo',
+        date: '21 May 2024',
+        comment: 'I am very happy with the look of my roof. The waiting period for the permits was long but once approved the workers were quick and efficient. Konstantinos answered my calls and questions I made. Thank you 😊',
+        rate: 3
+      }
+    ]
+  },
+  button: {
+    label: 'Reach Out To Us Now!',
+    cssSelector: 'form-contact'
+  }
+}
