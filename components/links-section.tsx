b2f886c90daf57e1c4f9373a6155b4bf31ff14a1
AntonioMPSS <110626235+AntonioMPSS@users.noreply.github.com>
2024-12-15 11:19:27 -0600

    feat: footer styles done

--- components/links-section.tsx
+++ components/links-section.tsx
@@ -22,10 +22,10 @@ export default function LinksSection({ logo, links, contact, social }: Props) {
 
   return (
     <section className="links-section py-[100px] bg-white w-full flex text-[14px] md:text-[15px] lg:text-[16px] font-normal text-black justify-center">
-      <div className="flex basis-9/12 w-9/12 justify-between">
-        <div className="contact-info flex flex-col basis-3/12 w-3/12">
+      <div className="flex flex-col items-center lg:flex-row basis-11/12 w-11/12 lg:basis-9/12 lg:w-9/12 justify-between">
+        <div className="contact-info flex flex-col items-center lg:items-start w-full basis-full lg:basis-4/12 lg:w-4/12">
           {logo && (
-            <div className="mt-[20px] lg:mt-0 w-full pr-[10px]">
+            <div className="flex justify-center lg:justify-start mt-[20px] lg:mt-0 w-full pr-[10px]">
               <Image
                 className="rounded-lg object-cover h-full"
                 src={logo.url}
@@ -36,24 +36,27 @@ export default function LinksSection({ logo, links, contact, social }: Props) {
             </div>
           )}
           {contact && (
-            <div className="flex flex-col">
+            <div className="flex flex-col items-center lg:items-start pt-[10px]">
+              <p className="flex transition-all items-center py-[5px] space-x-2 hover:text-forestgreen">
+                {contact.hashtag}
+              </p>
               <a
                 href={`tel:${contact.phoneNumber?.href}`}
-                className="flex items-center space-x-1"
+                className="flex transition-all items-center py-[5px] space-x-2 hover:text-forestgreen"
               >
                 <FaPhone />
                 <span>{contact.phoneNumber?.label}</span>
               </a>
               <a
                 href={`mailto:${contact.email?.emailHref}`}
-                className="flex items-center space-x-1"
+                className="flex transition-all items-center py-[5px] space-x-2 hover:text-forestgreen"
               >
                 <FaEnvelope />
                 <span>{contact.email?.label}</span>
               </a>
               <a
                 href={contact.address?.href}
-                className="flex items-center space-x-1"
+                className="flex transition-all items-center py-[5px] space-x-2 hover:text-forestgreen"
               >
                 <FaMapMarkerAlt />
                 <span>{contact.address?.label}</span>
@@ -61,28 +64,47 @@ export default function LinksSection({ logo, links, contact, social }: Props) {
             </div>
           )}
           {social && (
-            <div className="flex">
-              <a href={social.facebook} className="flex bg-forestgreen">
-                <FaFacebook className="bg-white" />
-              </a>
-              <a href={social.yelp} className="flex bg-forestgreen">
-                <FaYelp className="bg-white" />
-              </a>
-              <a href={social.googlePlus} className="flex bg-forestgreen">
-                <FaGooglePlusG className="bg-white" />
-              </a>
+            <div className="flex pt-5 flex-wrap justify-center lg:justify-start h-full">
+              <div className="transition-all mr-2">
+                <a
+                  href={social.facebook}
+                  className="flex transition-all text-[20px] hover:text-[25px] text-white hover:text-forestgreen bg-forestgreen hover:bg-midnightblue p-[10px] rounded-full"
+                >
+                  <FaFacebook className="bg-transparent" />
+                </a>
+              </div>
+              <div className="transition-all mr-2">
+                <a
+                  href={social.yelp}
+                  className="flex transition-all text-[20px] hover:text-[25px] text-white hover:text-forestgreen bg-forestgreen hover:bg-midnightblue p-[10px] rounded-full"
+                >
+                  <FaYelp className="bg-transparent" />
+                </a>
+              </div>
+              <div className="transition-all mr-2">
+                <a
+                  href={social.googlePlus}
+                  className="flex transition-all text-[20px] hover:text-[25px] text-white hover:text-forestgreen bg-forestgreen hover:bg-midnightblue p-[10px] rounded-full"
+                >
+                  <FaGooglePlusG className="bg-transparent" />
+                </a>
+              </div>
             </div>
           )}
         </div>
-        <div className="links flex justify-between basis-7/12 w-7/12">
+        <div className="links flex justify-between w-full basis-full flex-wrap lg:basis-7/12 lg:w-7/12">
           {links &&
             links.map((link) => (
-              <div key={link.title} className="flex flex-col basis-4/12">
+              <div key={link.title} className="flex flex-col items-center pt-5 lg:pt-0 lg:items-start basis-full md:basis-4/12">
                 <p className="font-bold text-[18px] md:text-[20px] mb-7">
                   {link.title}
                 </p>
                 {link.links.map((info) => (
-                  <Link className="mb-5 font-medium" key={info.label} href={info.url}>
+                  <Link
+                    className="mb-5 font-medium hover:text-forestgreen transition-all"
+                    key={info.label}
+                    href={info.url}
+                  >
                     {info.label}
                   </Link>
                 ))}
