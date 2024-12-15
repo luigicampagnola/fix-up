2100335825db94a6fceb512bc0939f69c2b5e33c
AntonioMPSS <110626235+AntonioMPSS@users.noreply.github.com>
2024-12-15 11:49:26 -0600

    feat: footer part 2

--- components/bottom-bar.tsx
+++ components/bottom-bar.tsx
@@ -13,13 +13,33 @@ export default function BottomBar({
   copyright,
 }: Props) {
   return (
-    <section className="bg-white w-full flex text-[14px] md:text-[15px] lg:text-[16px] font-normal text-black justify-center">
-      <div>
-        {copyright && <p>{copyright}</p>}
-        {privacypolicy && (
-          <Link href={privacypolicy?.label}>{privacypolicy?.label}</Link>
+    <section className="bg-midnightblue w-full flex text-[14px] md:text-[15px] lg:text-[16px] font-normal text-black justify-center">
+      <div className="font-semibold w-11/12 basis-11/12 md:basis-10/12 md:w-10/12 lg:basis-9/12 lg:w-9/12 text-white flex flex-wrap md:flex-row items-center">
+        {copyright && (
+          <div className="w-full basis-full lg:basis-5/12 lg:w-5/12 py-2 md:py-3">
+            <p className="mb-2 md:mb-0">{copyright}</p>
+          </div>
         )}
-        {createdby && <a href={createdby.url}>{createdby.label}</a>}
+        {privacypolicy && (
+          <div className="w-full basis-full lg:basis-2/12 lg:w-2/12 py-2 md:py-3">
+            <Link
+              className="transition-all mb-2 md:mb-0 hover:text-forestgreen"
+              href={privacypolicy?.label}
+            >
+              {privacypolicy?.label}
+            </Link>
+          </div>
+        )}
+        {createdby && (
+          <div className="w-full basis-full lg:basis-5/12 lg:w-5/12 py-2 md:py-3 text-right">
+            <a
+              className="transition-all mb-2 md:mb-0 hover:text-forestgreen"
+              href={createdby.url}
+            >
+              {createdby.label}
+            </a>
+          </div>
+        )}
       </div>
     </section>
   );
