d81fc10f14230993211f9139e2aa31a4d3183dbf
AntonioMPSS <110626235+AntonioMPSS@users.noreply.github.com>
2024-12-15 11:59:09 -0600

    fix: for mobile and tablet version

--- components/bottom-bar.tsx
+++ components/bottom-bar.tsx
@@ -16,12 +16,12 @@ export default function BottomBar({
     <section className="bg-midnightblue w-full flex text-[14px] md:text-[15px] lg:text-[16px] font-normal text-black justify-center">
       <div className="font-semibold w-11/12 basis-11/12 md:basis-10/12 md:w-10/12 lg:basis-9/12 lg:w-9/12 text-white flex flex-wrap md:flex-row items-center">
         {copyright && (
-          <div className="w-full basis-full lg:basis-5/12 lg:w-5/12 py-2 md:py-3">
+          <div className="w-full basis-full lg:basis-5/12 lg:w-5/12 py-2 md:py-3 lg:text-start text-center">
             <p className="mb-2 md:mb-0">{copyright}</p>
           </div>
         )}
         {privacypolicy && (
-          <div className="w-full basis-full lg:basis-2/12 lg:w-2/12 py-2 md:py-3">
+          <div className="w-full basis-full lg:basis-2/12 lg:w-2/12 py-2 md:py-3 text-center lg:text-start">
             <Link
               className="transition-all mb-2 md:mb-0 hover:text-forestgreen"
               href={privacypolicy?.label}
@@ -31,7 +31,7 @@ export default function BottomBar({
           </div>
         )}
         {createdby && (
-          <div className="w-full basis-full lg:basis-5/12 lg:w-5/12 py-2 md:py-3 text-right">
+          <div className="w-full basis-full lg:basis-5/12 lg:w-5/12 py-2 md:py-3 text-center lg:text-right">
             <a
               className="transition-all mb-2 md:mb-0 hover:text-forestgreen"
               href={createdby.url}
