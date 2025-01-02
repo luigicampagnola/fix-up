5770fdf530adef5179be63077b994d9f8d98f526
AntonioMPSS <110626235+AntonioMPSS@users.noreply.github.com>
2025-01-02 16:51:15 -0600

    feat: financing program

--- components/map-section.tsx
+++ components/map-section.tsx
@@ -17,61 +17,59 @@ export default function MapSection({
   mapsData,
 }: MapSectionProps) {
   return (
-    <>
-      <section className="flex flex-col items-center overflow-hidden relative">
-        <div className="bg-platinum top-0 left-0 rounded-none w-full h-full absolute opacity-80" />
-        <div className="max-w-7xl basis-11/12 w-11/12 lg:basis-10/12 md:w-10/12 z-10 pt-12 pb-16 flex flex-col items-center lg:justify-start">
-          <div className="flex flex-col items-center">
-            <h1 className="font-bold text-midnightblue text-[30px] md:text-[50px] leading-none uppercase text-center pt-[50px] max-w-4xl">
-              {title} <span className="text-forestgreen">{subtitle}</span>
-            </h1>
-            {description && (
-              <div
-                className="text-center text-black py-7 text-[14px] md:text-[15px] lg:text-[16px] max-w-5xl"
-                dangerouslySetInnerHTML={{ __html: description }}
-              ></div>
-            )}
-            {mapsData && (
-              <div className="flex justify-center items-center flex-wrap">
-                {mapsData.map((map, index) => (
-                  <div
-                    key={`map-${index}`}
-                    className="flex flex-wrap overflow-hidden justify-center basis-full w-full bg-white border rounded-2xl p-5 lg:basis-5/12 lg:w-5/12 mb-4 lg:mb-0 lg:mx-3"
-                  >
-                    <CustomGoogleMap center={map.center} zoom={map.zoom} />
-                    {map.label && (
-                      <div className="z-10 w-full flex justify-center flex-col items-center">
-                        <Link
-                          className="text-black hover:text-forestgreen text-[16px] lg:text-[18px] font-bold pt-4"
-                          aria-label={map.label}
-                          href={map.link ? map.link : "/"}
-                        >
-                          {map.label}
-                        </Link>
-                        {map.mapLocations && (
-                          <div className="flex justify-center flex-wrap w-full basis-full md:w-10/12 md:basis-10/12 lg:w-11/12 lg:basis-11/12 pt-3">
-                            {map.mapLocations.map((location, index) => (
-                              <div
-                                key={location.label + index}
-                                className="flex items-center px-1 md:px-2 text-black hover:text-forestgreen text-[14px] md:text-[15px] lg:text-[16px]"
-                              >
-                                <FaMapMarkerAlt />
-                                <p className="pl-[2px] font-medium">
-                                  {location.label}
-                                </p>
-                              </div>
-                            ))}
-                          </div>
-                        )}
-                      </div>
-                    )}
-                  </div>
-                ))}
-              </div>
-            )}
-          </div>
+    <section className="flex flex-col items-center overflow-hidden relative">
+      <div className="bg-platinum top-0 left-0 rounded-none w-full h-full absolute opacity-80" />
+      <div className="max-w-7xl basis-11/12 w-11/12 lg:basis-10/12 md:w-10/12 z-10 pt-12 pb-16 flex flex-col items-center lg:justify-start">
+        <div className="flex flex-col items-center">
+          <h1 className="font-bold text-midnightblue text-[30px] md:text-[50px] leading-none uppercase text-center pt-[50px] max-w-4xl">
+            {title} <span className="text-forestgreen">{subtitle}</span>
+          </h1>
+          {description && (
+            <div
+              className="text-center text-black py-7 text-[14px] md:text-[15px] lg:text-[16px] max-w-5xl"
+              dangerouslySetInnerHTML={{ __html: description }}
+            ></div>
+          )}
+          {mapsData && (
+            <div className="flex justify-center items-center flex-wrap">
+              {mapsData.map((map, index) => (
+                <div
+                  key={`map-${index}`}
+                  className="flex flex-wrap overflow-hidden justify-center basis-full w-full bg-white border rounded-2xl p-5 lg:basis-5/12 lg:w-5/12 mb-4 lg:mb-0 lg:mx-3"
+                >
+                  <CustomGoogleMap center={map.center} zoom={map.zoom} />
+                  {map.label && (
+                    <div className="z-10 w-full flex justify-center flex-col items-center">
+                      <Link
+                        className="text-black hover:text-forestgreen text-[16px] lg:text-[18px] font-bold pt-4"
+                        aria-label={map.label}
+                        href={map.link ? map.link : "/"}
+                      >
+                        {map.label}
+                      </Link>
+                      {map.mapLocations && (
+                        <div className="flex justify-center flex-wrap w-full basis-full md:w-10/12 md:basis-10/12 lg:w-11/12 lg:basis-11/12 pt-3">
+                          {map.mapLocations.map((location, index) => (
+                            <div
+                              key={location.label + index}
+                              className="flex items-center px-1 md:px-2 text-black hover:text-forestgreen text-[14px] md:text-[15px] lg:text-[16px]"
+                            >
+                              <FaMapMarkerAlt />
+                              <p className="pl-[2px] font-medium">
+                                {location.label}
+                              </p>
+                            </div>
+                          ))}
+                        </div>
+                      )}
+                    </div>
+                  )}
+                </div>
+              ))}
+            </div>
+          )}
         </div>
-      </section>
-    </>
+      </div>
+    </section>
   );
 }
