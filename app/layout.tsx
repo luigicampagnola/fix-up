b1479dac02776ee79191cd799853a590587d41b2
luigicampagnola <gigicampagnola@gmail.com>
2025-01-03 03:42:18 -0600

    navbar needs fixing

--- app/layout.tsx
+++ app/layout.tsx
@@ -24,7 +24,7 @@ export default function RootLayout({
   return (
     <html lang="en" className={poppins.className}>
       <body>
-        {/* <NavBar /> */}
+        <NavBar />
         {children}
       </body>
     </html>
