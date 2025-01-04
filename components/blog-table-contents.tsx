d6a79562459737995d26943d2a75b9632c4d88aa
AntonioMPSS <110626235+AntonioMPSS@users.noreply.github.com>
2025-01-03 22:04:25 -0600

    feat: added table

--- components/blog-table-contents.tsx
+++ components/blog-table-contents.tsx
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import { BlogThemes } from "./blog-slug-section";
 
 export default function BlogTableContents({ blogThemes }: BlogThemes) {
@@ -5,8 +6,24 @@ export default function BlogTableContents({ blogThemes }: BlogThemes) {
     <section className="text-black">
       <div className="">
         <h1>Table Contents</h1>
+        <ol className="flex flex-col">
+          {blogThemes &&
+            blogThemes.map((theme, index) => (
+              <li key={theme.title + "-t" + index}>
+                <Link href={theme.cssSelector} aria-label={theme.title} className="hover:underline" >
+                  {theme.title}
+                </Link>
+                <ol>
+                  {theme && theme.blogTips.map((tip, jndex) => 
+                  <li key={tip.title + "-tip" + jndex}>
+                    <Link className="hover:underline" href={tip.cssSelector} aria-label={tip.title}>{tip.title}</Link>
+                  </li>
+                  )}
+                </ol>
+              </li>
+            ))}
+        </ol>
       </div>
-
     </section>
   );
 }
