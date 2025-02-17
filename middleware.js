c8f345caf66b0351c3bc741a6c698fda9b612003
Rosie-Maldonado <rosiefordummies@gmail.com>
2025-02-16 18:22:36 -0600

    fix middleware

--- middleware.js
+++ middleware.js
@@ -1,22 +1,21 @@
-// middleware.js
 import { NextResponse } from "next/server";
 
 export function middleware(req) {
-  const country = req.headers.get("x-vercel-ip-country");
-  const region = req.headers.get("x-vercel-ip-region");
+  const country = req.headers.get("x-vercel-ip-country") || "unknown";
+  const region = req.headers.get("x-vercel-ip-region") || "unknown";
 
-  // Permitir solo visitantes de Florida, USA
+  console.log(`Country: ${country}, Region: ${region}`); // Verificar en logs
+
   if (country === "US" && region === "FL") {
     return NextResponse.next();
   }
 
-  // Bloquear el acceso para el resto
   return new NextResponse("Acceso restringido: Solo visitantes de Florida.", {
     status: 403,
   });
 }
 
-// Aplicar middleware a todas las rutas
+// Aplica el middleware a todas las rutas
 export const config = {
   matcher: "/:path*",
 };
--- next.config.ts
+++ next.config.ts
@@ -4,35 +4,35 @@ const nextConfig: NextConfig = {
   images: {
     remotePatterns: [
       {
-        protocol: 'https',
-        hostname: 'cdn-ilbgjlh.nitrocdn.com',
-        port: '',
-        pathname: '/**',
+        protocol: "https",
+        hostname: "cdn-ilbgjlh.nitrocdn.com",
+        port: "",
+        pathname: "/**",
       },
       {
-        protocol: 'https',
-        hostname: 'cdn-ilbgjlh.nitrocdn.com',
-        pathname: '/**'
+        protocol: "https",
+        hostname: "cdn-ilbgjlh.nitrocdn.com",
+        pathname: "/**",
       },
       {
-        protocol: 'https',
-        hostname: 'fixuproofing.com',
-        pathname: '/**'
+        protocol: "https",
+        hostname: "fixuproofing.com",
+        pathname: "/**",
       },
       {
-        protocol: 'https',
-        hostname: 'amazing-fireworks-dd56623770.media.strapiapp.com',
-        pathname: '/**'
+        protocol: "https",
+        hostname: "amazing-fireworks-dd56623770.media.strapiapp.com",
+        pathname: "/**",
       },
       {
-        protocol: 'http',
-        hostname: 'localhost:1337'
+        protocol: "http",
+        hostname: "localhost:1337",
       },
       {
-        protocol: 'https',
-        hostname: 'maps.googleapis.com',
-        pathname: '/**'
-      }
+        protocol: "https",
+        hostname: "maps.googleapis.com",
+        pathname: "/**",
+      },
     ],
   },
 };
