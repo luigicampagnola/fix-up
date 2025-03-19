d74b6393a56a29fb30d9ff494609060109a9acbb
jamc96 <jamc.mejia@gmail.com>
2025-03-18 18:41:51 -0600

    feat: improving not found and og image generation

--- app/[[...slug]]/page.tsx
+++ app/[[...slug]]/page.tsx
@@ -1,14 +1,14 @@
-import { getPage } from "@/utils/api";
-import DynamicModule from "@/components/dynamic-module";
-import { notFound } from "next/navigation";
-import { ModuleData } from "@/components/types";
-import { Key } from "react";
-import { PageData } from "@/components/types";
-import LinksSection from "@/components/links-section";
-import BottomBar from "@/components/bottom-bar";
-import { bottomBarSection, linkSection } from "@/utils/mock-data";
-import { Metadata } from "next";
-import Head from "next/head";
+import { getPage } from '@/utils/api';
+import DynamicModule from '@/components/dynamic-module';
+import { notFound } from 'next/navigation';
+import { ModuleData } from '@/components/types';
+import { Key } from 'react';
+import { PageData } from '@/components/types';
+import LinksSection from '@/components/links-section';
+import BottomBar from '@/components/bottom-bar';
+import { bottomBarSection, linkSection } from '@/utils/mock-data';
+import { Metadata } from 'next';
+import Head from 'next/head';
 
 interface PageProps {
   params: Promise<{ slug?: string[] }>;
@@ -18,21 +18,30 @@ export async function generateMetadata({
   params,
 }: PageProps): Promise<Metadata> {
   const resolvedParams = await params;
-  const slug = "/" + (resolvedParams?.slug?.join("/") || "");
+  const slug = '/' + (resolvedParams?.slug?.join('/') || '');
   const pageData: PageData | null = await getPage(slug);
 
   if (pageData) {
     const seoInformation = pageData.modules.find(
-      (module: ModuleData) => module.__component === "shared.seo"
+      (module: ModuleData) => module.__component === 'shared.seo'
     );
 
     const headTitle =
       seoInformation?.metaTitle ||
-      "Best Roofing and Construction Services | Fix Up Roofing";
+      'Best Roofing and Construction Services | Fix Up Roofing';
     const headDescription =
       seoInformation?.metaDescription ||
       "Choose Miami's top Best Roofing & Construction Company! Affordable, quick, and reliable solutions for your home or business.";
-    const headImage = seoInformation?.shareImage?.url || "/icon/fixup.svg";
+
+    // OG Image generation
+    const headImage =
+      seoInformation?.shareImage?.url ||
+      'https://www.fixuproofing.com/_next/image?url=https%3A%2F%2Famazing-fireworks-dd56623770.media.strapiapp.com%2Fo_48_c6deefdea5.jpg&w=1200&q=75';
+    const baseDomain =
+      process.env.NEXT_PUBLIC_BASE_URL || 'https://www.fixuproofing.com'; // Use env for domain
+    const optimizedImageUrl = `${baseDomain}/_next/image?url=${encodeURIComponent(
+      headImage
+    )}&w=1200&q=75`;
 
     // Generar la URL canónica
     const canonicalUrl =
@@ -42,7 +51,12 @@ export async function generateMetadata({
       title: headTitle,
       description: headDescription,
       openGraph: {
-        images: headImage,
+        images: [
+          {
+            url: `/api/og?title=${encodeURIComponent(headTitle)}
+            &imageUrl=${encodeURIComponent(optimizedImageUrl)}`,
+          },
+        ],
         title: headTitle,
         description: headDescription,
       },
@@ -52,22 +66,15 @@ export async function generateMetadata({
     };
   }
 
-  const headTitle = "Best Roofing and Construction Services | Fix Up Roofing";
+  const headTitle = 'Best Roofing and Construction Services | Fix Up Roofing';
   const headDescription =
     "Choose Miami's top Best Roofing & Construction Company! Affordable, quick, and reliable solutions for your home or business.";
-  const headImage = "/icon/fixup.svg";
-
   // Generar la URL canónica para la página predeterminada
   const canonicalUrl = `https://www.fixuproofing.com${slug}`;
 
   return {
     title: headTitle,
     description: headDescription,
-    openGraph: {
-      images: headImage,
-      title: headTitle,
-      description: headDescription,
-    },
     alternates: {
       canonical: canonicalUrl, // URL canónica para la página predeterminada
     },
@@ -76,7 +83,7 @@ export async function generateMetadata({
 
 export default async function DynamicPage({ params }: PageProps) {
   const resolvedParams = await params;
-  const slug = "/" + (resolvedParams?.slug?.join("/") || "");
+  const slug = '/' + (resolvedParams?.slug?.join('/') || '');
   const pageData: PageData | null = await getPage(slug);
 
   if (!pageData) {
@@ -88,7 +95,7 @@ export default async function DynamicPage({ params }: PageProps) {
   return (
     <>
       <Head>
-        <link rel="canonical" href={canonicalUrl} />{" "}
+        <link rel='canonical' href={canonicalUrl} />{' '}
         {/* Aquí se agrega el link canonical */}
       </Head>
       <div>
---
+++ app/api/og/route.tsx
@@ -0,0 +1,62 @@
+import { ImageResponse } from 'next/og';
+import { NextRequest } from 'next/server';
+
+export const dynamic = 'force-dynamic'; // Ensure dynamic rendering
+
+export async function GET(request: NextRequest) {
+  const { searchParams } = new URL(request.url);
+  const rawTitle = searchParams.get('title')!;
+  // const rawImageUrl = searchParams.get('imageUrl');
+
+  const splitTitle = rawTitle?.split('|');
+  const imageUrl =
+    searchParams.get('imageUrl') ||
+    'https://www.fixuproofing.com/_next/image?url=https%3A%2F%2Famazing-fireworks-dd56623770.media.strapiapp.com%2Fo_48_c6deefdea5.jpg&w=1200&q=75';
+  try {
+    return new ImageResponse(
+      (
+        <div
+          tw='flex w-full h-full relative'
+          style={{
+            backgroundImage: `url(${imageUrl})`, // Fixed typo
+            backgroundSize: 'cover',
+            backgroundPosition: 'center',
+          }}
+        >
+          <div
+            tw='flex flex-col w-full h-full items-center justify-center relative'
+            style={{ backgroundColor: 'rgba(19, 18, 69, 0.8)' }}
+          >
+            <div tw='flex flex-col md:flex-row w-full h-full md:items-center justify-between p-8'>
+              <div tw='flex flex-col text-4xl sm:text-6xl font-bold items-center tracking-tight text-left w-full'>
+                <span tw='text-white uppercase text-center'>
+                  {splitTitle[0]}
+                </span>
+                <span tw='text-[#539544] uppercase'>{splitTitle[1]}</span>
+                <a tw='mt-8 flex items-center justify-center rounded-md border border-transparent bg-[#539544] px-5 py-3 text-4xl text-white uppercase'>
+                  Contact Us Today!
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+      ),
+      {
+        width: 1200,
+        height: 630,
+      }
+    );
+  } catch (error) {
+    console.error('ImageResponse Error:', error);
+    return new ImageResponse(
+      (
+        <div tw='flex w-full h-full items-center justify-center bg-red-500'>
+          <span tw='text-white text-4xl'>
+            Error: Could not load background image
+          </span>
+        </div>
+      ),
+      { width: 1200, height: 630 }
+    );
+  }
+}
