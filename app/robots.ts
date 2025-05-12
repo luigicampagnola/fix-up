91242453e972fb94fa323448607088c7dfba4d03
jamc96 <jamc.mejia@gmail.com>
2025-05-12 05:33:40 -0600

    feat: not indexing old staging pages

--- app/robots.ts
+++ app/robots.ts
@@ -16,6 +16,7 @@ export default function robots(): MetadataRoute.Robots {
           '/graphql',
           '/staging',
           '/testing',
+          'stagingoct',
         ],
       },
       {
